163b25858d36c176af4809e862c29f37a5dbff22
Heitor Antonio Botelho Tupinamba <951131@senacminas.edu.br>
2026-02-26 11:52:24 -0300

    Criação de novos arquivos

---
+++ aula1.py
@@ -0,0 +1,7 @@
+#COMENTANDO QUALQUER COISA SÓ PARA TESTAR
+
+nome_produto = "Notebook"
+quantidade = 2
+preco_unitario = 3500.52
+
+print(f"Sua compra de {quantidade} {nome_produto} deu um total de R$ {(preco_unitario * quantidade):.1f}. Obrigado por comprar conosco!")
